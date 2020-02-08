3d3c6a7457162a4ef3c1c3bef55213cb4e1a4f2f
Satoshi Roppongi <satoshiroppongi@gmail.com>
2020-02-09 01:31:21 +0900

    add header and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <nuxt />
+    <Header />
+    <main>
+      <nuxt />
+    </main>
+    <Footer />
   </div>
 </template>
+<script>
+import Header from '@/components/header.vue'
+import Footer from '@/components/footer.vue'
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
 
 <style>
 html {

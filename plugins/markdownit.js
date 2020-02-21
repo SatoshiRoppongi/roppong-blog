993d2e5705d8c8596f73fb2a7e6b19666c0f4d5e
Satoshi Roppongi <satoshiroppongi@gmail.com>
2020-02-21 18:11:26 +0900

    目次表示を可能にした

---
+++ plugins/markdownit.js
@@ -0,0 +1,49 @@
+import MarkdownIt from 'markdown-it'
+import markdownItAnchor from 'markdown-it-anchor'
+import markdownItTocDoneRight from 'markdown-it-toc-done-right'
+/* refs https://izm51.com/posts/markdown-it-target-blank-anchor */
+
+export default ({ app }, inject) => {
+  const md = new MarkdownIt({
+    // markdownitのオプション
+    injected: true, // $mdを利用してmarkdownをhtmlにレンダリングする
+    breaks: true, // 改行コードを<br>に変換する
+    html: true, // HTML タグを有効にする
+    linkify: true, // URLに似たテキストをリンクに自動変換する
+    typography: true, // 言語に依存しないきれいな 置換 + 引用符 を有効にします。
+    preset: 'default',
+    xhtmlOut: true,
+    langPrefix: 'language-'
+  })
+  md.use(require('markdown-it-table-of-contents'))
+  md.use(require('markdown-it-footnote'))
+  md.use(require('markdown-it-mark'))
+  md.use(require('markdown-it-video'))
+  // md.use(require('markdown-it-anchor'))
+  // md.use(require('markdown-it-table-of-contents'))
+  md.use(markdownItAnchor, {
+    permalink: true,
+    permalinkBefore: true,
+    permalinkSymbol: '' // §
+  }).use(markdownItTocDoneRight)
+
+  const defaultRender =
+    md.renderer.rules.link_open ||
+    function(tokens, idx, options, env, self) {
+      return self.renderToken(tokens, idx, options)
+    }
+
+  md.renderer.rules.link_open = function(tokens, idx, options, env, self) {
+    const aIndex = tokens[idx].attrIndex('target')
+    if (tokens[idx].attrs[0][1].match('http')) {
+      if (aIndex < 0) {
+        tokens[idx].attrPush(['target', '_blank'])
+      } else {
+        tokens[idx].attrs[aIndex][1] = '_blank'
+      }
+    }
+    return defaultRender(tokens, idx, options, env, self)
+  }
+
+  inject('md', md)
+}

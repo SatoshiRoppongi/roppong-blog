1ba7ea78a4a2a7aa0f330638c6eeee54292ddfe3
Satoshi Roppongi <satoshiroppongi@gmail.com>
2020-02-24 21:26:42 +0900

    footerをシンプルに

--- components/footer.vue
+++ components/footer.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
+  <div class="footerwrapper">
     <footer>
-      <b-jumbotron
-        header="BootstrapVue"
-        lead="Bootstrap v4 Components for Vue.js 2"
-      >
-        <p>For more information visit website</p>
-        <b-button variant="primary" href="#">More Info</b-button>
-      </b-jumbotron>
+      <p>(c) 2020 Satoshi Roppongi</p>
     </footer>
   </div>
 </template>
+<style>
+.footerwrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 50vh;
+}
+
+footer {
+  margin-top: auto;
+  text-align: center;
+  background-color: #17a2b8;
+  padding: 30px 0;
+  color: #fff;
+  /*
+  width: 100%;
+  background-color: #89c7de;
+  color: #fff;
+  text-align: center;
+  padding: 30px 0;
+  position: absolute; // ←絶対位置
+  bottom: 0; // 下に固定
+  */
+}
+</style>

e09500ab7a904d7e5b0748d4d87783b6e8b3e85e
Satoshi Roppongi <satoshiroppongi@gmail.com>
2020-07-29 07:55:48 +0900

    change header logo

--- components/header.vue
+++ components/header.vue
@@ -4,9 +4,13 @@
       <b-card
         bg-variant="dark"
         text-variant="white"
-        title="roppong blog"
         class="text-center rounded-0"
       >
+        <b-card-body>
+          <nuxt-link to="/blog">
+            <b-img src="~/static/logo.png"> </b-img>
+          </nuxt-link>
+        </b-card-body>
       </b-card>
       <HeaderNav />
     </header>

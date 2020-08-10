44a46a4bf2ab87504d44eb427356de2b9771aa9c
Satoshi Roppongi <satoshiroppongi@gmail.com>
2020-08-10 22:58:57 +0900

    improve landing page a little bit (stillNotSatisfy

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="wrap">
+  <b-jumbotron class="wrap" fluid container-fluid text-variant="white">
+    <template v-slot:header>roppong.com</template>
+    <template v-slot:lead>
+      Wanna check my blog? Go to here below
+    </template>
+    <b-button variant="primary" href="blog">roppong blog</b-button>
     <video
       id="bg-video"
       src="landing.mp4"
-      poster="https://picsum.photos/900/300/?random"
+      poster="landing_page_img.png"
       autoplay
       loop
       muted
     ></video>
-    <div class="header-title">
-      <b-jumbotron header="roppong.com" lead="Inside my brain">
-        <p>Wanna check my blog? Go to here below</p>
-        <b-button variant="primary" href="blog">roppong blog</b-button>
-      </b-jumbotron>
-    </div>
+    <b-img
+      id="bg-img-for-mobile"
+      src="landing_page_img.png"
+      fluid
+      alt="background image"
+    >
+    </b-img>
     <p id="copyright">(c)2020 Satoshi Roppongi</p>
-  </div>
+  </b-jumbotron>
 </template>
 
 <script></script>
@@ -23,27 +29,17 @@
 #bg-video {
   position: fixed;
   right: 0;
+  top: 0;
   bottom: 0;
   min-width: 100%;
   min-height: 100%;
   width: auto;
   height: auto;
   z-index: -100;
-  background: url('https://picsum.photos/900/300/?random') no-repeat;
-  background-size: cover;
-}
-.header-title {
-  position: relative;
-  z-index: 2;
-  display: flex;
-  width: 50%;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  margin: auto;
 }
 .wrap {
-  height: 100vh;
+  min-width: 100%;
+  min-height: 100vh;
   background: linear-gradient(
     -45deg,
     rgba(4, 114, 124, 0.4),
@@ -53,6 +49,9 @@
   justify-content: center;
   align-items: center;
 }
+#bg-img-for-mobile {
+  display: none;
+}
 
 #copyright {
   position: absolute;
@@ -61,5 +60,22 @@
   text-align: center;
   width: 80%;
   color: aliceblue;
+}
+
+@media screen and (max-device-width: 420px) {
+  #bg-video {
+    display: none;
+  }
+  #bg-img-for-mobile {
+    position: fixed;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    min-width: 500%;
+    width: auto;
+    height: auto;
+    z-index: -100;
+    display: block;
+  }
 }
 </style>

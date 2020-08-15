72534e9440e91d92ad87dae5a75f77afe706613b
Satoshi Roppongi <satoshiroppongi@gmail.com>
2020-08-15 17:28:21 +0900

    change header logo

--- components/header.vue
+++ components/header.vue
@@ -8,8 +8,9 @@
       >
         <b-card-body>
           <nuxt-link to="/blog">
-            <b-img-lazy src="~/static/logo.png" fluid alt="roppong.com logo">
-            </b-img-lazy>
+            <h1>
+              roppong blog
+            </h1>
           </nuxt-link>
         </b-card-body>
       </b-card>
@@ -25,3 +26,22 @@ export default {
   }
 }
 </script>
+<style scoped>
+html {
+  background: #ccd0d4;
+}
+h1 {
+  text-align: center;
+  font-family: 'Nunito', sans-serif;
+  font-size: 3.375em;
+  color: transparent;
+  letter-spacing: 0.1em;
+  text-shadow: 0 0 2px rgba(204, 208, 212, 0.9), 0 15px 25px rgba(0, 0, 0, 0.3),
+    0 -2px 3px rgba(0, 0, 0, 0.1), 0 -5px 10px rgba(255, 255, 255, 0.5),
+    0 5px 10px rgba(0, 0, 0, 0.3), 0 3px 4px rgba(255, 255, 255, 0.2),
+    0 0 20px rgba(255, 255, 255, 0.45);
+}
+a:hover {
+  text-decoration: none;
+}
+</style>
